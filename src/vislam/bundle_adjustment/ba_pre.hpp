27602bc643f18985357a1e4db5392a5cd0c7b910
Kazuki Kikuchi <ery.eden@gmail.com>
2020-02-17 22:52:00 +0900

    mod ba code

--- src/vislam/bundle_adjustment/ba_pre.hpp
+++ src/vislam/bundle_adjustment/ba_pre.hpp
@@ -4,3 +4,25 @@
 
 #pragma once
 
+#include "camera.hpp"
+#include "frame.hpp"
+#include "landmark.hpp"
+
+namespace vislam::ba{
+
+    /**
+     * @brief 役割：パラメータの初期化以外のBA処理全体を実装するクラス
+     * @details
+     * となると、特に中間データも必要ない？Utilityクラスとしての実装になるかもしれない。
+     * ## 入力と出力について
+     * - 入力：frame_database, landmark_database, sliding_window size?
+     */
+    class ba_pre{
+
+
+
+
+    };
+
+}
+
