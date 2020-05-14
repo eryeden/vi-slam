0e2942d327c83398460869a357758db599996f6d
KK <ery.eden@gmail.com>
2020-05-15 01:08:13 +0900

    Implement ANMS detection filter. #17

--- example/vslam/example_kimera_frontend_detection_and_tracking.cpp
+++ example/vslam/example_kimera_frontend_detection_and_tracking.cpp
@@ -7,6 +7,7 @@
 
 #include "EurocKimeraDataProvider.hpp"
 #include "EurocKimeraDataProviderRadialTangentialCameraModel.hpp"
+#include "FeatureDetectorANMS.hpp"
 #include "FeatureDetectorShiTomasiBucketing.hpp"
 #include "FeatureTrackerLucasKanade.hpp"
 #include "KimeraFrontend.hpp"
@@ -20,11 +21,11 @@ int main() {
   //      euroc_kimera_data_provider(path_to_euroc);
 
   // EUROC
-  //  std::string path_to_euroc =
-  //      "/home/ery/subspace/docker_work/dataset/V1_01_easy";
-  //  std::string path_to_calibfile =
-  //      "/home/ery/subspace/docker_work/dataset/basalt_calib/euroc_calib/"
-  //      "calib_results/calibration.json";
+  //    std::string path_to_euroc =
+  //        "/home/ery/subspace/docker_work/dataset/V1_01_easy";
+  //    std::string path_to_calibfile =
+  //        "/home/ery/subspace/docker_work/dataset/basalt_calib/euroc_calib/"
+  //        "calib_results/calibration.json";
 
   std::string path_to_euroc =
       "/home/ery/subspace/docker_work/dataset/dataset-corridor1_512_16";
@@ -35,21 +36,21 @@ int main() {
   vslam::dataprovider::EurocKimeraDataProvider euroc_kimera_data_provider(
       path_to_euroc, path_to_calibfile);
 
-  //  std::string path_to_euroc =
-  //      "/e/subspace/docker_work/dataset/fukuroi/camlog_2020-05-13-21-09-39/";
-  //  std::string path_to_calibfile =
-  //      "/e/subspace/docker_work/dataset/fukuroi/calib_result/calibration.json";
-  //  std::string path_to_mask =
-  //  "/e/subspace/docker_work/dataset/fukuroi/calib_result/vingette_0.png";
-  //  vslam::dataprovider::EurocKimeraDataProvider euroc_kimera_data_provider(
-  //      path_to_euroc, path_to_calibfile, path_to_mask);
-
-  spdlog::info(
-      "Load dataset from:\n"
-      "Dataset : {}\n"
-      "Calibfile : {}",
-      path_to_euroc,
-      path_to_calibfile);
+  //    std::string path_to_euroc =
+  //        "/e/subspace/docker_work/dataset/fukuroi/camlog_2020-05-13-21-09-39/";
+  //    std::string path_to_calibfile =
+  //        "/e/subspace/docker_work/dataset/fukuroi/calib_result/calibration.json";
+  //    std::string path_to_mask =
+  //    "/e/subspace/docker_work/dataset/fukuroi/calib_result/vingette_0.png";
+  //    vslam::dataprovider::EurocKimeraDataProvider euroc_kimera_data_provider(
+  //        path_to_euroc, path_to_calibfile, path_to_mask);
+  //
+  //  spdlog::info(
+  //      "Load dataset from:\n"
+  //      "Dataset : {}\n"
+  //      "Calibfile : {}",
+  //      path_to_euroc,
+  //      path_to_calibfile);
 
   // For output
   //  cv::VideoWriter video_writer("feature_tracking_and_detection.mp4",
@@ -65,6 +66,9 @@ int main() {
   auto shi_tomasi_detector_ptr =
       std::make_shared<vslam::feature::FeatureDetectorShiTomasiBucketing>(
           2, 2, 200, 5.0);
+
+  auto anms_detector_ptr =
+      std::make_shared<vslam::feature::FeatureDetectorANMS>(200, 10.0);
 
   // Build tracker
   auto kl_tracker_ptr =
@@ -92,12 +96,14 @@ int main() {
   //      100.0,
   //      100);
 
-  vslam::frontend::KimeraFrontend kimera_frontend(threadsafe_map_database_ptr,
-                                                  shi_tomasi_detector_ptr,
-                                                  kl_tracker_ptr,
-                                                  verification_ptr,
-                                                  10.0,
-                                                  60);
+  vslam::frontend::KimeraFrontend kimera_frontend(
+      threadsafe_map_database_ptr,
+      //                                                  shi_tomasi_detector_ptr,
+      anms_detector_ptr,
+      kl_tracker_ptr,
+      verification_ptr,
+      10.0,
+      100);
 
   vslam::data::FrameSharedPtr prev_frame = nullptr;
   vslam::FeatureAgeDatabase prev_feature_age;
--- src/vslam/feature/FeatureDetectorANMS.hpp
+++ src/vslam/feature/FeatureDetectorANMS.hpp
@@ -8,6 +8,7 @@
 
 #include "FeatureDetectorBase.hpp"
 #include "Frame.hpp"
+#include "NonMaximumSuppression/NonMaximumSuppression.hpp"
 
 namespace vslam::feature {
 
@@ -15,7 +16,7 @@ enum class FeatureDetectorType { FAST, ORB, AGAST, GFTT };
 
 class FeatureDetectorANMS : public FeatureDetectorBase {
  public:
-  FeatureDetectorANMS();
+  FeatureDetectorANMS(int32_t max_feature_number, double min_feature_distance);
 
   void UpdateDetection(FeaturePositionDatabase& feature_position,
                        FeatureAgeDatabase& feature_age,
@@ -23,6 +24,23 @@ class FeatureDetectorANMS : public FeatureDetectorBase {
                        const cv::Mat& mask_image = cv::Mat()) override;
 
  private:
+  void Detect(FeaturePositionDatabase& feature_position,
+              FeatureAgeDatabase& feature_age,
+              const cv::Mat& current_image,
+              int32_t max_feature_number,
+              double min_feature_distance,
+              database_index_t& max_feature_index,
+              const cv::Mat& mask_image = cv::Mat()) const;
+
+  database_index_t max_feature_index_;
+
+  int32_t max_feature_number_;
+  double min_feature_distance_;
+
+  std::unique_ptr<NonMaximumSuppression> non_max_suppression_;
+
+  // Actual feature detector implementation.
+  cv::Ptr<cv::Feature2D> feature_detector_;
 };
 
 }  // namespace vslam::feature
