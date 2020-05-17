20756334dc8f8d79f634fe03d3ae0c070d224c97
KK <ery.eden@gmail.com>
2020-05-17 12:12:14 +0900

    Add GTSAM deps and a sample code.

---
+++ example/gtsam/example_gtsam_test1.cpp
@@ -0,0 +1,26 @@
+//
+// Created by ery on 2020/05/17.
+//
+
+#include <fmt/format.h>
+#include <gtsam/geometry/Cal3DS2.h>
+#include <gtsam/geometry/Cal3_S2.h>
+#include <gtsam/geometry/StereoCamera.h>
+#include <gtsam/geometry/StereoPoint2.h>
+#include <gtsam/navigation/CombinedImuFactor.h>
+#include <gtsam/navigation/ImuFactor.h>
+#include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/nonlinear/LinearContainerFactor.h>
+#include <gtsam/nonlinear/Marginals.h>
+#include <gtsam/nonlinear/NonlinearFactorGraph.h>
+#include <gtsam/slam/BetweenFactor.h>
+#include <gtsam/slam/PriorFactor.h>
+#include <gtsam_unstable/nonlinear/BatchFixedLagSmoother.h>
+#include <gtsam_unstable/slam/SmartStereoProjectionPoseFactor.h>
+#include <spdlog/spdlog.h>
+
+/**
+ * @brief Just include and build.
+ */
+
+int main() { spdlog::info("Hello GTSAM."); }
