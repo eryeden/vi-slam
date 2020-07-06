b1ffe5cb37b25b2084a32646517d310e22cac57b
KK <ery.eden@gmail.com>
2020-07-07 07:45:15 +0900

    Generate histogram of parallaxes and baseline lengths.

--- scripts/track_vslam.py
+++ scripts/track_vslam.py
@@ -24,7 +24,6 @@ import evo.common_ape_rpe as ape_rpe
 
 import matplotlib.pyplot as plt
 
-
 def get_path_to_reference_trajectory(path_to_result_directory):
     dataset_config = open(path_to_result_directory + "/EurocKimeraDataProvider.json", 'r')
     json_dataset_config = json.load(dataset_config)
@@ -50,6 +49,33 @@ def get_all_frame_internals_json(path_to_result_directory):
         j = json.load(j_string)
         frame_internals.append(j["value0"])
     return frame_internals
+
+
+def get_lm_database(path_to_result_directory):
+    lm_database_json = open(path_to_result_directory + "/Mapdatabase.json", 'r')
+    return json.load(lm_database_json)["value0"]
+
+
+def get_lm_database_parallax_angle(lm_database):
+    parallax_angles = []
+    for lm in lm_database:
+        # print(lm)
+        is_initialized = bool(lm["value"]["is_initialized"]["atomic_data"])
+        is_added = bool(lm["value"]["is_added"]["atomic_data"])
+        if is_initialized:
+            parallax_angles.append(float(lm["value"]["triangulate_parallax_angle"]["atomic_data"]))
+    return parallax_angles
+
+
+def get_lm_database_base_line_length(lm_database):
+    base_line_lens = []
+    for lm in lm_database:
+        # print(lm)
+        is_initialized = bool(lm["value"]["is_initialized"]["atomic_data"])
+        is_added = bool(lm["value"]["is_added"]["atomic_data"])
+        if is_initialized:
+            base_line_lens.append(float(lm["value"]["triangulate_baseline_length"]["atomic_data"]))
+    return base_line_lens
 
 
 def get_inuse_landmark_number(json_input):
@@ -191,6 +217,29 @@ if __name__ == "__main__":
         frame_internals = get_all_frame_internals_json(path_to_tmp_directory)
         metrics = extract_metrics(frame_internals)
 
+        # Evaluate trajectory
+        # import evo.common_ape_rpe as common
+        # from evo.core import sync
+        # from evo.tools import file_interface, log
+        # from evo.core import metrics
+        # from evo.core import trajectory
+        # from evo.core.metrics import PoseRelation
+        # from evo.main_ape import ape
+        # traj_estimated = PoseTrajectory3D(result["traj_position"], result["traj_orientation"], result["traj_timestamp"])
+        # traj_reference_raw = file_interface.read_euroc_csv_trajectory(get_path_to_reference_trajectory(path_to_tmp_directory))
+        # traj_ref, traj_est = sync.associate_trajectories(traj_reference_raw, traj_estimated, 0.01, 0.0,first_name="reference", snd_name="estimate")
+        # pose_relation = PoseRelation.translation_part
+        # result = ape(
+        #     traj_ref=traj_ref,
+        #     traj_est=traj_est,
+        #     pose_relation=pose_relation,
+        #     align=True,
+        #     correct_scale=True,
+        #     align_origin=True,
+        #     ref_name="reference",
+        #     est_name="estimate",
+        # )
+
         # Log input params
         path_to_dataset_root = get_path_to_dataset(path_to_tmp_directory)
         log_param("Dataset", os.path.basename(path_to_dataset_root))
@@ -236,9 +285,32 @@ if __name__ == "__main__":
         plt.savefig(path_to_tmp_directory + "/triangulate_takeover_rate.png", dpi=300)
         log_artifact(path_to_tmp_directory + "/triangulate_takeover_rate.png")
 
+        # LM stat
+        lm_database = get_lm_database(path_to_tmp_directory)
+        parallax = get_lm_database_parallax_angle(lm_database)
+        baselength = get_lm_database_base_line_length(lm_database)
+        fig_hist_triangulate_parallax = plt.figure(7)
+        plt.hist(parallax, bins=100)
+        plt.axvline(max(parallax))
+        plt.axvline(min(parallax))
+        plt.xlabel("Triangulate parallax [rad]")
+        plt.ylabel("Frequency")
+        plt.savefig(path_to_tmp_directory + "/hist_triangulate_parallax.png", dpi=300)
+        log_artifact(path_to_tmp_directory + "/hist_triangulate_parallax.png")
+        fig_hist_baseline = plt.figure(8)
+        plt.hist(baselength, bins=50)
+        plt.xlabel("Triangulate baseline length")
+        plt.ylabel("Frequency")
+        plt.savefig(path_to_tmp_directory + "/hist_triangulate_baseline_length.png", dpi=300)
+        log_artifact(path_to_tmp_directory + "/hist_triangulate_baseline_length.png")
+
         log_artifact(path_to_tmp_directory + "/EurocKimeraDataProvider.json")
         log_artifact(path_to_tmp_directory + "/FeatureDetectorANMS.json")
         log_artifact(path_to_tmp_directory + "/FeatureTrackerLSSDLucasKanade.json")
         log_artifact(path_to_tmp_directory + "/FeatureVerification5PointRANSAC.json")
         log_artifact(path_to_tmp_directory + "/iSAM2Backend.json")
         log_artifact(path_to_tmp_directory + "/trajectory_body_frame.tum")
+        log_artifact(path_to_tmp_directory + "/Mapdatabase.json")
+        mlflow.end_run()
+
+        plt.show()
