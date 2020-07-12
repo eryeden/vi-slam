ed8069a52b57960d6a6c5a27b2c489d4a2f46c08
Kazuki Kikuchi <ery.eden@gmail.com>
2020-07-12 09:40:02 +0900

    Add a support for subPC.

--- scripts/launch_mlflow_server.sh
+++ scripts/launch_mlflow_server.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mlflow server --backend-store-uri /e/subspace/docker_work/dataset/result/tracking --default-artifact-root /e/subspace/docker_work/dataset/result/tracking --host 0.0.0.0
+#mlflow server --backend-store-uri /e/subspace/docker_work/dataset/result/tracking \
+#--default-artifact-root /e/subspace/docker_work/dataset/result/tracking \
+#--host 0.0.0.0
+
+mlflow server --backend-store-uri /home/ery/devel/dataset/result/tracking \
+--default-artifact-root /home/ery/devel/dataset/result/tracking \
+--host 0.0.0.0
